9f02509787c294eaee82169d9c7ad1cc943941a0
Luis Moreno <luis.moreno@edunext.co>
2020-04-22 20:36:06 -0400

    Get LOGIN_REDIRECT_WHITELIST from SiteConfigurations

--- common/djangoapps/student/utils.py
+++ common/djangoapps/student/utils.py
@@ -6,6 +6,7 @@ from urlparse import urlparse
 from django.conf import settings
 from django.utils import http
 
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
 def is_safe_login_or_logout_redirect(request, redirect_to):
     """
@@ -13,7 +14,7 @@ def is_safe_login_or_logout_redirect(request, redirect_to):
     """
     request_host = request.get_host()  # e.g. 'courses.edx.org'
 
-    login_redirect_whitelist = set(getattr(settings, 'LOGIN_REDIRECT_WHITELIST', []))
+    login_redirect_whitelist = set(configuration_helpers.get_value('LOGIN_REDIRECT_WHITELIST', settings.LOGIN_REDIRECT_WHITELIST))
     login_redirect_whitelist.add(request_host)
 
     is_safe_url = http.is_safe_url(redirect_to, allowed_hosts=login_redirect_whitelist, require_https=True)
