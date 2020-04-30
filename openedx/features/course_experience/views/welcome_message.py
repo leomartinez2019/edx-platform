eduNEXT/edunext-platform

allowing to show welcome message using settings

openedx/features/course_experience/views/welcome_message.py
@@ -2,6 +2,7 @@
 View logic for handling course welcome messages.
 """
 
+from django.conf import settings
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.template.loader import render_to_string
@@ -42,7 +43,9 @@ class WelcomeMessageFragmentView(EdxFragmentView):
             'welcome_message_html': welcome_message_html,
         }
 
-        if get_course_tag(request.user, course_key, PREFERENCE_KEY) == 'False':
+        # HA-24 added by edunext in order to disable the permanent welcome message dismiss
+        if (not settings.FEATURES.get('EDNX_DISABLE_DISMISS_WELCOME_MESSAGE', False) and
+                get_course_tag(request.user, course_key, PREFERENCE_KEY) == 'False'):
             return None
         else:
             html = render_to_string('course_experience/welcome-message-fragment.html', context)

leomartinez2019/edx-platform

openedx/features/course_experience/views/welcome_message.py
@@ -2,6 +2,7 @@
 View logic for handling course welcome messages.
 """
 
+from django.conf import settings
 from django.urls import reverse
 from django.http import HttpResponse
 from django.template.loader import render_to_string
@@ -42,7 +43,9 @@ class WelcomeMessageFragmentView(EdxFragmentView):
             'welcome_message_html': welcome_message_html,
         }
 
-        if get_course_tag(request.user, course_key, PREFERENCE_KEY) == 'False':
+        # HA-24 added by edunext in order to disable the permanent welcome message dismiss
+        if (not settings.FEATURES.get('EDX_DISABLE_DISMISS_WELCOME_MESSAGE', False) and
+                get_course_tag(request.user, course_key, PREFERENCE_KEY) == 'False'):
             return None
         else:
             html = render_to_string('course_experience/welcome-message-fragment.html', context)